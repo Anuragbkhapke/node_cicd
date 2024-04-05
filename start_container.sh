7d57e7c49ec2f8909f54e921b13a81c47e66860a
Anuragbkhapke <128586795+Anuragbkhapke@users.noreply.github.com>
2024-04-05 13:08:06 +0530

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-set -e
-
 # Pull the Docker image from ecr
 sudo aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 531574450554.dkr.ecr.ap-south-1.amazonaws.com
 
