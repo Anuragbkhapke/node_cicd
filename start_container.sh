09a5f2615958e38a2591ac38f877a9c1f0983b35
Anuragbkhapke <128586795+Anuragbkhapke@users.noreply.github.com>
2024-04-05 12:15:02 +0530

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -7,3 +7,4 @@ sudo aws ecr get-login-password --region ap-south-1 | docker login --username AW
 docker pull 531574450554.dkr.ecr.ap-south-1.amazonaws.com/nodeimage:latest
 
 # Run the Docker image as a container
+docker run -d -p 531574450554.dkr.ecr.ap-south-1.amazonaws.com/nodeimage:latest
